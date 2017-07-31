dec0f0b87d664bab7e4c5842387226160c1c9235
Andrew Stoddart <11509490+andystod@users.noreply.github.com>
2017-07-31 16:47:24 +0100

    add nav bar and table components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,33 @@
-import { ApolloClient } from 'apollo-client';
-import { ApolloModule } from 'apollo-angular';
+// import { ApolloClient } from 'apollo-client';
+// import { ApolloModule } from 'apollo-angular';
 
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { routes } from "./app.router";
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
+import { routes } from "./app.router";
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { TableComponent } from './table/table.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 // Create the client as outlined above
-const client = new ApolloClient();
+// const client = new ApolloClient();
 
-export function provideClient(): ApolloClient {
-  return client;
-}
+// export function provideClient(): ApolloClient {
+//   return client;
+// }
 
 @NgModule({
   imports: [
     BrowserModule,
-    ApolloModule.forRoot(provideClient),
-    routes
+    // ApolloModule.forRoot(provideClient),
+    routes,
+    NgbModule
   ],
-  declarations: [ AppComponent, HomeComponent, TableComponent ],
+  declarations: [ AppComponent, HomeComponent, TableComponent, NavbarComponent ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule {}
--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TableComponent implements OnInit {
 
+  tableRows: Array<TableRow>;
+
   constructor() { }
 
   ngOnInit() {
+    this.tableRows = [];
+
+    var tableRow = new TableRow();
+    tableRow.position = 1;
+    tableRow.user = 'Andrew Stoddart';
+    tableRow.gameweekPick = 'Manchester United';
+    tableRow.gameweekPoints = 3;
+    tableRow.totalPoints = 30;
+    this.tableRows.push(tableRow);
   }
 
+// ToDo: Pass id???
+  tableRowClick(tableRow) {
+    alert(tableRow.user);
+  }
+
+}
+
+export class TableRow {
+  position: number;
+  user: string;
+  gameweekPick: string;
+  gameweekPoints: number;
+  totalPoints: number; 
 }
