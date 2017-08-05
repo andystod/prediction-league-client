57d248665bbf5860711c5d2ae2ce9111de93f7ad
Andrew Stoddart <11509490+andystod@users.noreply.github.com>
2017-08-05 19:18:32 +0100

    get apollo client working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,9 @@
-// import { ApolloClient } from 'apollo-client';
-// import { ApolloModule } from 'apollo-angular';
+import { ApolloClient, createNetworkInterface } from 'apollo-client';
+import { ApolloModule } from 'apollo-angular';
 
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { routes } from "./app.router";
 import { AppComponent } from './app.component';
@@ -13,19 +11,24 @@ import { HomeComponent } from './home/home.component';
 import { TableComponent } from './table/table.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
-// Create the client as outlined above
-// const client = new ApolloClient();
+// by default, this client will send queries to `/graphql` (relative to the URL of your app)
+const client = new ApolloClient({
+  networkInterface: createNetworkInterface({
+    uri: 'http://localhost:4000/graphql'
+  }),
+});
 
-// export function provideClient(): ApolloClient {
-//   return client;
-// }
+
+export function provideClient(): ApolloClient {
+  return client;
+}
+
 
 @NgModule({
   imports: [
     BrowserModule,
-    // ApolloModule.forRoot(provideClient),
-    routes,
-    NgbModule
+    ApolloModule.forRoot(provideClient),
+    routes
   ],
   declarations: [ AppComponent, HomeComponent, TableComponent, NavbarComponent ],
   bootstrap: [ AppComponent ]
--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,4 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
+
+// We use the gql tag to parse our query string into a query document
+const CurrentUserForProfile = gql`
+  query TableQuery {
+    table {
+      id,
+      user {
+        name,
+        predictions {
+          name
+        }
+      },
+      gameweekPick {
+        name
+      },
+      gameweekPoints,
+      totalPoints,
+      position
+    }
+  }
+`;
+
+interface QueryResponse{
+  table
+  loading
+}
+
 
 @Component({
   selector: 'app-table',
@@ -6,21 +35,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./table.component.css']
 })
 export class TableComponent implements OnInit {
-
+  loading: boolean;
+  table: any;
   tableRows: Array<TableRow>;
 
-  constructor() { }
+  constructor(private apollo: Apollo) { }
 
   ngOnInit() {
-    this.tableRows = [];
+    
+    console.log('here');
+    console.log('here');
 
-    var tableRow = new TableRow();
-    tableRow.position = 1;
-    tableRow.user = 'Andrew Stoddart';
-    tableRow.gameweekPick = 'Manchester United';
-    tableRow.gameweekPoints = 3;
-    tableRow.totalPoints = 30;
-    this.tableRows.push(tableRow);
+    this.apollo.watchQuery<QueryResponse>({
+      query: CurrentUserForProfile
+    }).subscribe(({data}) => {
+      console.log(data);
+      this.loading = data.loading;
+      this.table = data.table;
+
+    //   this.tableRows = [];
+
+    // var tableRow = new TableRow();
+    // tableRow.position = 1;
+    // tableRow.user = 'Andrew Stoddart';
+    // tableRow.gameweekPick = 'Manchester United';
+    // tableRow.gameweekPoints = 3;
+    // tableRow.totalPoints = 30;
+    // this.tableRows.push(tableRow);
+
+      console.log('finished');
+    });
+
   }
 
 // ToDo: Pass id???
